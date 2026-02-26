8825113cb05c2ed49bc327a3f8d8abe59e485916
JoelP654 <joel.patterson@kingsway.school.nz>
2026-02-27 12:21:27 +1300

    Able to parse expressions

--- src/lib/latexParser.js
+++ src/lib/latexParser.js
@@ -1,3 +1,5 @@
+import jsonExpressions from "./expressions.json" with { type: 'json' }
+
 class Token {
     constructor(stringValue, tokenType) {
         this.stringValue = stringValue //String
@@ -6,16 +8,38 @@ class Token {
     }
 
     parseStringValue() {
-        const [preComment, comment] = seperateByFirstOccurence(this.stringValue, "%")
-        const expressions = preComment.split(" ")
-        expressions.forEach(expression => {
-            if (expression != "") {
-                this.childTokens.push(new Token(expression, "expression"))
-            }
-        })
-        this.childTokens.push(new Token(comment, "comment"))
-    }
+        switch (this.tokenType) {
 
+            case "line": {
+                const [preComment, comment] = seperateByFirstOccurence(this.stringValue, "%")
+                const chunks = preComment.split(" ")
+                chunks.forEach(chunk => {
+                    if (chunk != "") {
+                        const newToken = new Token(chunk, "chunk")
+                        newToken.parseStringValue()
+                        this.childTokens.push(newToken)
+                    }
+                })
+                this.childTokens.push(new Token(comment, "comment"))
+                break
+            }
+
+            case "chunk": {
+                // Recognise expressions
+                const [before, ...expressions] = this.stringValue.split("\\")
+                const newTokens = [new Token(before, "value")]
+                expressions.forEach(expression => {
+                    if (!jsonExpressions.includes(expression)) {
+                        newTokens.push(new Token(("\\backslash " + expression), "value"))
+                        // throw new Error("Unrecognised expression: " + expression)
+                    } else {
+                        newTokens.push(new Token((expression), "expression"))
+                    }
+                this.childTokens = newTokens
+                })
+            }
+        }
+    }
 }
 
 export const seperateByFirstOccurence = (string, seperator) => {
@@ -25,13 +49,16 @@ export const seperateByFirstOccurence = (string, seperator) => {
 }
 
 export const parseLatex = (rawInput) => {
-    const lines = rawInput.split("\n")
-    const tokens = []
-    lines.forEach(line => {
-        tokens.push(new Token(line, "line"))
-    });
-    tokens.forEach(token => {
-        token.parseStringValue()
-    })
-    console.log(tokens)
+    console.log(rawInput)
+    if (rawInput != "") {
+        var lines = rawInput.split("\n").join("\\\\").split("\\\\").join("\\newline").split("\\newline")
+        const tokens = []
+        lines.forEach(line => {
+            tokens.push(new Token(line, "line"))
+        });
+        tokens.forEach(token => {
+            token.parseStringValue()
+        })
+        console.log(tokens)
+    }
 }
