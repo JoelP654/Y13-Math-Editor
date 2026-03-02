f2b19e8445f35a8a81a5697ff42cdeb2e623f5c8
JoelP654 <joel.patterson@kingsway.school.nz>
2026-03-02 14:01:53 +1300

    Parser rework

--- src/lib/latexParser.js
+++ src/lib/latexParser.js
@@ -1,64 +1,74 @@
-import jsonExpressions from "./expressions.json" with { type: 'json' }
+// import jsonExpressions from "./expressions.json" with { type: 'json' }
 
 class Token {
     constructor(stringValue, tokenType) {
-        this.stringValue = stringValue //String
-        this.tokenType = tokenType //String
-        this.childTokens = [] //Array
-    }
-
-    parseStringValue() {
-        switch (this.tokenType) {
-
-            case "line": {
-                const [preComment, comment] = seperateByFirstOccurence(this.stringValue, "%")
-                const chunks = preComment.split(" ")
-                chunks.forEach(chunk => {
-                    if (chunk != "") {
-                        const newToken = new Token(chunk, "chunk")
-                        newToken.parseStringValue()
-                        this.childTokens.push(newToken)
-                    }
-                })
-                this.childTokens.push(new Token(comment, "comment"))
-                break
-            }
-
-            case "chunk": {
-                // Recognise expressions
-                const [before, ...expressions] = this.stringValue.split("\\")
-                const newTokens = [new Token(before, "value")]
-                expressions.forEach(expression => {
-                    if (!jsonExpressions.includes(expression)) {
-                        newTokens.push(new Token(("\\backslash " + expression), "value"))
-                        // throw new Error("Unrecognised expression: " + expression)
-                    } else {
-                        newTokens.push(new Token((expression), "expression"))
-                    }
-                this.childTokens = newTokens
-                })
-            }
-        }
+        this.stringValue = stringValue
+        this.tokenType = tokenType
     }
 }
 
-export const seperateByFirstOccurence = (string, seperator) => {
+const seperateByFirstOccurence = (string, seperator) => {
     const [before, ...rest] = string.split(seperator)
     const after = rest.join(seperator)
     return [before, after]
 }
 
-export const parseLatex = (rawInput) => {
-    console.log(rawInput)
-    if (rawInput != "") {
-        var lines = rawInput.split("\n").join("\\\\").split("\\\\").join("\\newline").split("\\newline")
-        const tokens = []
-        lines.forEach(line => {
-            tokens.push(new Token(line, "line"))
-        });
-        tokens.forEach(token => {
-            token.parseStringValue()
-        })
-        console.log(tokens)
+const parseLines = (input) => {
+    if (input != "") {
+        var lines = input.split("\n").join("\\\\").split("\\\\").join("\\newline").split("\\newline")
+        return lines
+    } else {
+        return []
     }
+}
+
+// Returns array of length two, with before and after comment
+const parseComment = (input) => {
+    if (input.includes("%")) {
+        const [preComment, comment] = seperateByFirstOccurence(input, "%")
+        return {
+            "comment": comment,
+            "preComment": preComment
+        }
+    } else {
+        return {
+            "comment": "",
+            "preComment": input
+        }
+    }
+}
+
+// Returns an array of expressions
+const parseChunk = (input) => {
+    var values = []
+    var valueBuffer = []
+
+    for (var i = 0; i < input.length; i++) {
+        if (input[i] == " ") {
+            values.push(valueBuffer.join(""))
+            valueBuffer = []
+        } else {
+            valueBuffer.push(input[i])
+        }
+    }
+    values.push(valueBuffer.join(""))
+    return values
+}
+
+// Returns an array of tokens, minimal nesting
+export const parseLatex = (input) => {
+    var lines = parseLines(input)
+    var tokens = []
+    lines.forEach(line => {
+        var result = parseComment(line)
+        console.log(result)
+        var expressions = parseChunk(result.preComment)
+        expressions.forEach(expression => {
+            tokens.push(new Token(expression, "expression"))
+        })
+        if (result.comment != "") {
+            tokens.push(new Token(result.comment, "comment"))
+        }
+    })
+    console.log(tokens)
 }
