f1e63caf1cc872a0aa338d1efd79f64445d70cb5
JoelP654 <joel.patterson@kingsway.school.nz>
2026-05-07 10:41:15 +1200

    Save feature commented

--- src/components/saveScreens.jsx
+++ src/components/saveScreens.jsx
@@ -1,29 +1,21 @@
+// Joel Patterson
+// 7/5/26
+// Save screens
+// This contains react components to 
+
+// Imports
 import { FaTrash } from "react-icons/fa"
 import { useEffect, useState } from "react"
 import { getItem, removeSave } from "../lib/saveFunctions"
 import { MdCancel } from "react-icons/md"
 
-
-function SaveComponent({ id, title, latex, date, openSave, close }) {
-
-    return (
-        <div className="saveComponent">
-            {title}
-            {"Last modified: " + (new Date(date)).toLocaleDateString()}
-            <FaTrash icon={"trash-can"} onClick={() => { removeSave(id) }}/>
-            <div
-                onClick={() => { openSave(id, title, latex); close() }}
-            >
-                Open
-            </div>
-        </div>
-    )
-}
-
+// The screen that appears to open saves.
 export function OpenSaveScreen({ close, openSave }) {
+    // State variables
     const [saves, setSaves] = useState([])
-    const [update, setUpdate] = useState(0)
+    const [update, setUpdate] = useState(0) // This is used as a cheat to refresh saves
 
+    // On mount or when updating, get the saves
     useEffect(() => {
         const getSaves = () => {
             const get = getItem("saves", [])
@@ -34,15 +26,25 @@ export function OpenSaveScreen({ close, openSave }) {
 
     return (
         <div className="saveModal">
+
+            {/* Top bar */}
             <div>
                 <h1>Load Save</h1>
                 <MdCancel icon={"cancel"} onClick={close}/>
             </div>
+
+            {/* For each save, render the save */}
             {saves && saves.map((save) => (
                 <div className="saveComponent" key={save.id}>
+
+                    {/* Save information */}
                     {save.title}
                     {"Last modified: " + (new Date(save.date)).toLocaleDateString()}
+                    
+                    {/* Delete save button */}
                     <FaTrash icon={"trash-can"} onClick={() => { removeSave(save.id); setUpdate(update + 1) }}/>
+                    
+                    {/* Open save button */}
                     <div
                         onClick={() => { openSave(save.id, save.title, save.latex); close() }}
                     >
@@ -50,6 +52,8 @@ export function OpenSaveScreen({ close, openSave }) {
                     </div>
                 </div>
             ))}
+
+            {/* If no saves, display message */}
             {saves.length == 0 &&
                 <div>No saves found</div>
             }
@@ -57,20 +61,28 @@ export function OpenSaveScreen({ close, openSave }) {
     )
 }
 
+// Component to create a new save
 export function SaveScreen({ close, save, title }) {
+
+    // State variables
     const [inputTitle, setInputTitle] = useState(title)
     const [message, setMessage] = useState("")
 
+    // When title changes or on mount, set the input bar's value to the title
     useEffect(() => {
         setInputTitle(title);
     }, [title]);
 
     return (
         <div className="saveModal">
+
+            {/* Top bar */}
             <div>
                 <h1>Save</h1>
                 <MdCancel icon={"cancel"} onClick={close}/>
             </div>
+
+            {/* Input bar */}
             <input
                 value={inputTitle}
                 onChange={(e) => {
@@ -78,18 +90,23 @@ export function SaveScreen({ close, save, title }) {
                     setMessage("")
                 }}
             />
+
+            {/* Save button */}
             <div onClick={() => {
+                // If valid title, save and close
                 if (inputTitle != "") {
                     save(inputTitle)
                     close()
                 }
+                // Otherwise, set warning message
                 else {
                     setMessage("Enter a Valid Save Name")
                 }
-                
             }}>
                 Save
             </div>
+
+            {/* Display message */}
             <div>
                 {message}
             </div>
--- src/lib/saveFunctions.js
+++ src/lib/saveFunctions.js
@@ -1,3 +1,9 @@
+// Joel Patterson
+// 7/5/26
+// Save functions
+// This file contains functions to handle and manage saving
+
+// Basic class to use as container for save values
 export class Save {
     constructor(id, title, latex, date) {
         this.id = id
@@ -7,22 +13,27 @@ export class Save {
     }
 }
 
+// Gets item from local storage, defaulting to value def if nothing is found
 export const getItem = (key, def) => {
     let data = JSON.parse(localStorage.getItem(key))
     if (data) {return data}
     else {return def}
 }
+
+// Sets item in local storage to value, defaulting to value def if no value is set
 export const setItem = (key, value, def) => {
     if (value) {localStorage.setItem(key, JSON.stringify(value))}
     else {localStorage.setItem(key, JSON.stringify(def))}
 }
 
+// Removes save (based on ID) by filtering it out
 export const removeSave = (id) => {
     let saves = getItem("saves", [])
     saves = saves.filter((save) => save.id != id)
     setItem("saves", saves, [])
 }
 
+// Adds new save. Removes any previous saves with the same ID to overwrite
 export const addSave = (id, title, input) => {
     removeSave(id)
     const newSave = new Save(id, title, input, new Date)
